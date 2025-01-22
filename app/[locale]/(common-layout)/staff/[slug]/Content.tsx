26048a625d0e4876c23366b0721c03b010f6d398
Kyle Kingsbury <kyle_kingsbury@epam.com>
2025-01-22 13:48:20 -0500

    Added staff publication tabs

--- app/[locale]/(common-layout)/staff/[slug]/Content.tsx
+++ app/[locale]/(common-layout)/staff/[slug]/Content.tsx
@@ -1,7 +1,3 @@
-import QuoteBlock from "@/components/QuoteBlock";
-
-import DatoImage from "@/components/DatoImage";
-import type { ContentPage } from "@/components/WithRealTimeUpdates/types";
 import {
   isBlockquote,
   isHeading,
@@ -13,15 +9,17 @@ import {
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { StructuredText, renderNodeRule } from "react-datocms";
-import type { PageProps, Query } from "./meta";
-import { buttonVariants, Text } from "@/components/ui";
 import clsx from "clsx";
 import { EnvelopeOpenIcon } from "@radix-ui/react-icons";
+import QuoteBlock from "@/components/QuoteBlock";
+import DatoImage from "@/components/DatoImage";
+import type { ContentPage } from "@/components/WithRealTimeUpdates/types";
+import type { PageProps, Query } from "./meta";
+import { buttonVariants, Text } from "@/components/ui";
 import { cn } from "@/components/utils";
+import { PublicationTabs } from "./_components/Tabs";
 
-const Content: ContentPage<PageProps, Query> = ({
-  data,
-}) => {
+const Content: ContentPage<PageProps, Query> = ({ data }) => {
   if (!data.author) {
     notFound();
   }
@@ -30,134 +28,143 @@ const Content: ContentPage<PageProps, Query> = ({
     <section className="bg-theme-canvas">
       <div className="container py-12 md:py-24 bg-white">
         <div className="flex flex-wrap justify-center">
-          <div className="w-full px-4 lg:w-8/12">
-            <div>
-              <div className="mb-5 gap-4 flex flex-col sm:flex-row sm:items-center">
-                <div className="">
-                  <div className="relative w-full md:w-[240px] md:h-[240px]">
-                    <DatoImage
-                      className="h-full w-full object-cover"
-                      fragment={data.author.picture.responsiveImage}
-                    />
-                  </div>
-                </div>
-                <div className="w-full space-y-3 sm:space-y-4">
-                  <Text
-                    as="h1"
-                    weight="semi"
-                    className="text-heading sm:text-heading break-words"
-                  >
-                    {data.author.name}
-                  </Text>
-                  <Text size="meta" className="break-words">
-                    {data.author.jobTitle}
-                  </Text>
-                  {data.author.email && (
-                    <Text caps size="meta">
-                      <Link
-                        href={`mailto:${data.author.email}`}
-                        className="flex break-words"
-                      >
-                        <EnvelopeOpenIcon className="inline mr-3" />
-                        {data.author.email}
-                      </Link>
-                    </Text>
-                  )}
+          <div className="w-full px-4 lg:w-8/12 space-y-6">
+            <div className="gap-4 flex flex-col sm:flex-row sm:items-center">
+              <div className="">
+                <div className="relative w-full md:w-[240px] md:h-[240px]">
+                  <DatoImage
+                    className="h-full w-full object-cover"
+                    fragment={data.author.picture.responsiveImage}
+                  />
                 </div>
               </div>
-              <div className="space-y-4">
-                <StructuredText
-                  data={data.author.biography}
-                  customNodeRules={[
-                    renderNodeRule(isHeading, ({ children, key, node }) => {
-                      return (
-                        <Text
-                          as={`h${node.level}`}
-                          className={clsx([
-                            "mb-4 mt-9",
-                            { ["text-2xl"]: node.level === 2 },
-                            { ["text-xl"]: node.level === 3 },
-                            { ["text-lg"]: node.level === 4 },
-                            { ["text-sm"]: node.level === 5 },
-                            { ["text-meta"]: node.level === 6 },
-                          ])}
-                          key={key}
-                          weight="bold"
-                        >
-                          {children}
-                        </Text>
-                      );
-                    }),
-                    renderNodeRule(isList, ({ children, key, node }) => {
-                      const Node = node.style === "bulleted" ? "ul" : "ol";
-                      return (
-                        <Node
-                          className={clsx([
-                            "ps-6 mt-4 marker:text-base",
-                            { ["list-disc"]: node.style === "bulleted" },
-                            { ["list-decimal"]: node.style !== "bulleted" },
-                          ])}
-                          key={key}
-                        >
-                          {children}
-                        </Node>
-                      );
-                    }),
-                    renderNodeRule(isListItem, ({ children, key, node }) => {
-                      return (
-                        <li key={key} className="mb-4">
-                          {children}
-                        </li>
-                      );
-                    }),
-                    renderNodeRule(isParagraph, ({ children, key }) => {
-                      return (
-                        <Text as="p" size="base" key={key}>
-                          {children}
-                        </Text>
-                      );
-                    }),
-                    renderNodeRule(isLink, ({ node, children, key }) => {
-                      const attributeObject =
-                        node.meta?.reduce((acc, { id, value }) => {
-                          acc[id] = value;
-                          return acc;
-                        }, {} as Record<string, string>) || {};
-
-                      return (
-                        <Link
-                          className="text-base font-medium leading-relaxed text-body-color underline sm:text-lg sm:leading-relaxed"
-                          href={node.url}
-                          key={key}
-                          {...attributeObject}
-                        >
-                          {children}
-                        </Link>
-                      );
-                    }),
-                    renderNodeRule(isBlockquote, ({ children, key }) => {
-                      return <QuoteBlock key={key}>{children}</QuoteBlock>;
-                    }),
-                  ]}
-                />
-                {data.author.cvLink?.url && (
-                  <p className="text-center">
+              <div className="w-full space-y-3 sm:space-y-4">
+                <Text
+                  as="h1"
+                  weight="semi"
+                  className="text-heading sm:text-heading break-words"
+                >
+                  {data.author.name}
+                </Text>
+                <Text size="meta" className="break-words">
+                  {data.author.jobTitle}
+                </Text>
+                {data.author.email && (
+                  <Text caps size="meta">
                     <Link
-                      href={data.author.cvLink.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className={cn(
-                        buttonVariants({ variant: "outline", size: "lg" })
-                      )}
+                      href={`mailto:${data.author.email}`}
+                      className="flex break-words"
                     >
-                      <Text as="span" size="meta" caps align="center">
-                        Download CV
-                      </Text>
+                      <EnvelopeOpenIcon className="inline mr-3" />
+                      {data.author.email}
                     </Link>
-                  </p>
+                  </Text>
                 )}
               </div>
             </div>
+            <div className="space-y-4">
+              <StructuredText
+                data={data.author.biography}
+                customNodeRules={[
+                  renderNodeRule(isHeading, ({ children, key, node }) => {
+                    return (
+                      <Text
+                        as={`h${node.level}`}
+                        className={clsx([
+                          "mb-4 mt-9",
+                          { ["text-2xl"]: node.level === 2 },
+                          { ["text-xl"]: node.level === 3 },
+                          { ["text-lg"]: node.level === 4 },
+                          { ["text-sm"]: node.level === 5 },
+                          { ["text-meta"]: node.level === 6 },
+                        ])}
+                        key={key}
+                        weight="bold"
+                      >
+                        {children}
+                      </Text>
+                    );
+                  }),
+                  renderNodeRule(isList, ({ children, key, node }) => {
+                    const Node = node.style === "bulleted" ? "ul" : "ol";
+                    return (
+                      <Node
+                        className={clsx([
+                          "ps-6 mt-4 marker:text-base",
+                          { ["list-disc"]: node.style === "bulleted" },
+                          { ["list-decimal"]: node.style !== "bulleted" },
+                        ])}
+                        key={key}
+                      >
+                        {children}
+                      </Node>
+                    );
+                  }),
+                  renderNodeRule(isListItem, ({ children, key, node }) => {
+                    return (
+                      <li key={key} className="mb-4">
+                        {children}
+                      </li>
+                    );
+                  }),
+                  renderNodeRule(isParagraph, ({ children, key }) => {
+                    return (
+                      <Text as="p" size="base" key={key}>
+                        {children}
+                      </Text>
+                    );
+                  }),
+                  renderNodeRule(isLink, ({ node, children, key }) => {
+                    const attributeObject =
+                      node.meta?.reduce(
+                        (acc, { id, value }) => {
+                          acc[id] = value;
+                          return acc;
+                        },
+                        {} as Record<string, string>
+                      ) || {};
+
+                    return (
+                      <Link
+                        className="text-base font-medium leading-relaxed text-body-color underline sm:text-lg sm:leading-relaxed"
+                        href={node.url}
+                        key={key}
+                        {...attributeObject}
+                      >
+                        {children}
+                      </Link>
+                    );
+                  }),
+                  renderNodeRule(isBlockquote, ({ children, key }) => {
+                    return <QuoteBlock key={key}>{children}</QuoteBlock>;
+                  }),
+                ]}
+              />
+              {data.author.cvLink?.url && (
+                <p className="text-center">
+                  <Link
+                    href={data.author.cvLink.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={cn(
+                      buttonVariants({ variant: "outline", size: "lg" })
+                    )}
+                  >
+                    <Text as="span" size="meta" caps align="center">
+                      Download CV
+                    </Text>
+                  </Link>
+                </p>
+              )}
+            </div>
+            {data.author.publications && data.author.publications.length > 0 && (
+              <div className="pt-10 space-y-6">
+                <Text as="h2" weight="bold" size="heading">
+                  Publications
+                </Text>
+                <PublicationTabs publications={data.author.publications} />
+              </div>
+            )}
           </div>
         </div>
       </div>
