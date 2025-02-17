a768f43a08c7ed5a5e79e16f594c8aa9a7d0ca4f
Kyle Kingsbury <kyle_kingsbury@epam.com>
2025-02-17 14:44:07 -0500

    Added Mailchimp journeys to force Journeys to start

--- services/mailchimp.ts
+++ services/mailchimp.ts
@@ -6,14 +6,19 @@ export type AddContactRequest = {
   fields: Record<string, string>;
   tags?: string[];
 };
-
-export type AddContactResponse = {
-  success: boolean;
+export type TriggerJourneyRequest = {
+  journeyId: string;
+  stepId: string;
+  email: string;
 };
 
-const md5 = (key: string ): string => {
-  return createHash('md5').update(key).digest('hex');
+export interface MailchimpResponse {
+  success: boolean;
 }
+
+const md5 = (key: string): string => {
+  return createHash("md5").update(key).digest("hex");
+};
 
 /**
  * Adds a contact to a Mailchimp audience list.
@@ -23,7 +28,7 @@ const md5 = (key: string ): string => {
  * @param {string} params.email - The email address of the contact to add.
  * @param {Object} params.fields - The merge fields for the contact.
  * @param {string[]} [params.tags] - Optional tags to assign to the contact.
- * @returns {Promise<AddContactResponse>} A promise that resolves to the response of the add contact operation.
+ * @returns {Promise<MailchimpResponse>} A promise that resolves to the response of the add contact operation.
  * @throws {Error} If the input data is invalid or if the Mailchimp API returns an error.
  */
 export const addOrUpdateContact = async ({
@@ -31,10 +36,10 @@ export const addOrUpdateContact = async ({
   email,
   fields,
   tags,
-}: AddContactRequest): Promise<AddContactResponse> => {
+}: AddContactRequest): Promise<MailchimpResponse> => {
   if (!audienceId || !email || !fields || Object.keys(fields).length === 0) {
-    console.error("[mailchimp add-contact] Invalid data sent do addContact");
-    throw new Error("Invalid data passed to handler.");
+    console.error("[mailchimp addOrUpdateContact] Invalid parameters");
+    throw new Error("Invalid parameters passed to handler.");
   }
 
   try {
@@ -65,9 +70,58 @@ export const addOrUpdateContact = async ({
     }
 
     console.log(
-      `Success: added "${email}" to ${audienceId} with ${tags || []} tags.`,
-      responseBody
+      `Success: added "${email}" to ${audienceId} with ${tags || []} tags.`
     );
+    return {
+      success: true,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+/**
+ * Triggers a Mailchimp customer journey step for a given email address.
+ *
+ * @param {Object} params - The parameters for triggering the journey.
+ * @param {string} params.email - The email address to add to the journey.
+ * @param {string} params.journeyId - The ID of the journey to trigger.
+ * @param {string} params.stepId - The ID of the journey step to trigger.
+ * @returns {Promise<MailchimpResponse>} A promise that resolves to a MailchimpResponse object indicating success or failure.
+ * @throws {Error} If any of the required parameters are missing or if the Mailchimp API returns an error.
+ */
+export const triggerJourney = async ({
+  email,
+  journeyId,
+  stepId,
+}: TriggerJourneyRequest): Promise<MailchimpResponse> => {
+  if (!journeyId || !email || !stepId) {
+    console.error("[mailchimp triggerJourney] Invalid parameters");
+    throw new Error("Invalid parameters passed to handler.");
+  }
+
+  try {
+    const response = await fetch(
+      `https://us12.api.mailchimp.com/3.0/customer-journeys/journeys/${journeyId}/steps/${stepId}/actions/trigger`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `auth ${process.env.MAILCHIMP_API_KEY}`,
+        },
+        body: JSON.stringify({
+          email_address: email,
+        }),
+      }
+    );
+    const text = response.text();
+    const { ok, status } = response;
+
+    if (!ok || status !== 204) {
+      throw new Error(`Mailchimp API error: ${text}`);
+    }
+
+    console.log(`Success: added "${email}" to Journey ${journeyId}-${stepId}.`);
     return {
       success: true,
     };
